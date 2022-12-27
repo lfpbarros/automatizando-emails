b6c0d6690d581e63368bd67d7d710ace5a4fb88e
lfpbarros <lip.tjsba@gmail.com>
2022-12-27 18:04:41 -0300

    Mandando emails com texto em html e anexos

--- email_script.py
+++ email_script.py
@@ -10,7 +10,7 @@ email_origem = ''
 email_destino = ('')
 
 assunto = 'Orçamento de produtos'
-body = open('corpo_email.txt', 'r', encoding='utf-8').read()
+body = open('corpo_email_html.txt', 'r', encoding='utf-8').read()
 # o encoding é muito importante para corrigir a questão da acentuação.
 
 mensagem = EmailMessage()
@@ -19,10 +19,17 @@ mensagem['From'] = email_origem
 mensagem['To'] = email_destino
 mensagem['Subject'] = assunto
 
-mensagem.set_content(body)
+anexo_path = 'imagem.jpg'
+mime_type, mime_subtype = mimetypes.guess_type(anexo_path)[0].split('/') # carregando a imagem, o caminho precisa ser possível do python entender, daí há o split do primeiro valor e o atribuímos a duas variáveis
+
+mensagem.set_content(body, subtype='html')
 
 safe = ssl.create_default_context()
 # garente que o email seja criptografado com tecnologia ssl
+
+with open(anexo_path, 'rb') as ap:
+    mensagem.add_attachment(ap.read(), maintype=mime_type, subtype=mime_subtype, filename=anexo_path)
+    # adicionado o anexo a mensagem
 
 # quando abrimos alguma conexão com o python, precisamos, fechá-la
 # o with é uma estrutura do python que permite a abertura de conexões e ao final dela, essa conexão é encerrada 
